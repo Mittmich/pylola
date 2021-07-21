3396b5693b9ed0e8a1c0d68c018b8d3f5f3acf90
Mittmich <michael_mitter@hotmail.com>
2021-07-21 14:13:34 +0200

    added multiprocessing

--- pylola/utils.py
+++ pylola/utils.py
@@ -1,3 +1,5 @@
+from multiprocessing import Pool
+from functools import partial
 import bioframe as bf
 import pandas as pd
 import numpy as np
@@ -31,12 +33,15 @@ def _do_enrichment_analysis(row):
         "p_value": p_value
     }
 
-def run_lola(query, target_list, universe, name_series=None):
+def run_lola(query, target_list, universe, names=None, processes=2):
     """runs enrichment analysis on query with dataframes in target_list
     spanning the universe."""
-    contingency_frame = pd.DataFrame([_get_contingency_table(query, target, universe) for target in target_list])
+    with Pool(processes) as p:
+        contingency_list = p.map(partial(_get_contingency_table, query=query, universe=universe), target_list)
+    contingency_frame = pd.DataFrame(contingency_list)
     enrichment_result = contingency_frame.apply(_do_enrichment_analysis, axis=1, result_type="expand")
-    if name_series is None:
+    if names is None:
         return pd.concat((contingency_frame, enrichment_result), axis=1)
     else:
-        return pd.concat((contingency_frame, enrichment_result, name_series), axis=1)
+        name_frame = pd.DataFrame({"names": names})
+        return pd.concat((contingency_frame, enrichment_result, name_frame), axis=1)
